275d2606814b067c35e8e15b0d4856e14ad5f492
Owale128n <Christian.buaka@hotmail.com>
2024-01-11 19:19:40 +0100

    👽️ Cleaned up the code and added some structure by putting functions and interface in different folders

---
+++ src/functions/taskFunctions.ts
@@ -0,0 +1,61 @@
+import { Task } from "../interface/taskInterface";
+
+let taskList: Task[] = JSON.parse(localStorage.getItem('task') || JSON.stringify([]));
+
+export const saveTask = () => {
+    localStorage.setItem('task', JSON.stringify(taskList))
+};
+
+
+export const addTask = (task: string) => {
+   
+    if(task.trim() !== '') {
+        const todo = {task: task, completed: false}
+        taskList.push(todo)
+      saveTask()
+      upDateTask()
+    }  
+
+}
+
+export const upDateTask = () => {
+    const incomplete = document.querySelector('#incomplete-list') as HTMLElement;
+    const complete = document.querySelector('#complete-list') as HTMLElement;
+
+    incomplete.innerHTML = '';
+    complete.innerHTML = '';
+
+    taskList.forEach((todo, index) =>{
+        const taskItem = document.createElement('li');
+        taskItem.className = 'task-item';
+
+        taskItem.innerHTML= `<div class="checkmark">&#10004;</div>
+        ${todo.task}
+        <button class="delete-button">Delete Button</button>`;
+
+        taskItem.querySelector('.checkmark')?.addEventListener('click', ()=> toggleTask(index))
+        taskItem.querySelector('.delete-button')?.addEventListener('click', ()=> deleteTask(index))
+
+        if(todo.completed) {
+        taskItem.classList.add('checked')
+        complete.appendChild(taskItem)
+        } else {
+            incomplete.appendChild(taskItem)
+        }
+    })
+}
+
+
+export const toggleTask = (index: number) => {
+taskList[index].completed = !taskList[index].completed
+saveTask()
+upDateTask()
+}
+
+export const deleteTask = (index: number) => {
+taskList.splice(index, 1)
+saveTask()
+upDateTask()
+}
+
+upDateTask()
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,7 @@
-interface Task {
-    task: string;
-    completed: boolean;
-};
+import * as createTask from './functions/taskFunctions';
 
-let taskList: Task[] = JSON.parse(localStorage.getItem('task') || JSON.stringify([]));
 
-const saveTask = () => {
-    localStorage.setItem('task', JSON.stringify(taskList))
-};
+
 
 document.getElementById('form')?.addEventListener('submit', (e: SubmitEvent) =>{
     e.preventDefault();
@@ -15,53 +9,10 @@ document.getElementById('form')?.addEventListener('submit', (e: SubmitEvent) =>{
     const taskInput = document.getElementById('taskText') as HTMLInputElement;
     const taskInputValue = taskInput.value;
 
-    if(taskInputValue.trim() !== ''){
-        const todo = {task: taskInputValue, completed: false}
-        taskList.push(todo)
-        saveTask()
-        upDateTask()
-    }   
+    createTask.addTask(taskInputValue)
+ 
     taskInput.value = '';
 })
 
-const upDateTask = () => {
-    const incomplete = document.querySelector('#incomplete-list') as HTMLElement;
-    const complete = document.querySelector('#complete-list') as HTMLElement;
 
-    incomplete.innerHTML = '';
-    complete.innerHTML = '';
-
-    taskList.forEach((todo, index) =>{
-        const taskItem = document.createElement('li');
-        taskItem.className = 'task-item';
-
-        taskItem.innerHTML= `<div class="checkmark">&#10004;</div>
-        ${todo.task}
-        <button class="delete-button">Delete Button</button>`;
-
-        taskItem.querySelector('.checkmark')?.addEventListener('click', ()=> toggleTask(index))
-        taskItem.querySelector('.delete-button')?.addEventListener('click', ()=> deleteTask(index))
-
-        if(todo.completed) {
-        taskItem.classList.add('checked')
-        complete.appendChild(taskItem)
-        } else {
-            incomplete.appendChild(taskItem)
-        }
-    })
-}
-
-
-const toggleTask = (index: number) => {
-taskList[index].completed = !taskList[index].completed
-saveTask()
-upDateTask()
-}
-
-const deleteTask = (index: number) => {
-taskList.splice(index, 1)
-saveTask()
-upDateTask()
-}
-
-upDateTask()
+createTask.upDateTask()
