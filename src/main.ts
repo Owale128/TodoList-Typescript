327d8221106bfa106041640df8cd198541ce3495
Owale128n <Christian.buaka@hotmail.com>
2024-01-11 18:04:19 +0100

    👽️ Added a little scss to center everything and also started with some TS

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,23 @@
+interface Task {
+    task: string;
+    completed: boolean;
+};
 
+let taskList: Task[] = JSON.parse(localStorage.getItem('task') || JSON.stringify([]));
 
+const saveTask = () => {
+    localStorage.setItem('task', JSON.stringify(taskList))
+};
+
+document.getElementById('form')?.addEventListener('submit', (e: SubmitEvent) =>{
+    e.preventDefault();
+
+    const taskInput = document.getElementById('taskText') as HTMLInputElement;
+    const taskInputValue = taskInput.value;
+
+    if(taskInputValue.trim() !== ''){
+        const todo = {task: taskInputValue, completed: false}
+        taskList.push(todo)
+    }
+    taskInput.value = '';
+})
