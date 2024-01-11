8234cbeac45f6e61afc7a63826d6f72452f83dc1
Owale128n <Christian.buaka@hotmail.com>
2024-01-11 18:38:15 +0100

    👽️ Added an update function

--- src/main.ts
+++ src/main.ts
@@ -21,3 +21,28 @@ document.getElementById('form')?.addEventListener('submit', (e: SubmitEvent) =>{
     }
     taskInput.value = '';
 })
+
+const upDateTask = () => {
+    const incomplete = document.querySelector('#incomplete-list') as HTMLElement;
+    const complete = document.querySelector('#complete-list') as HTMLElement;
+
+    incomplete.innerHTML = '';
+    complete.innerHTML = '';
+
+    taskList.forEach((todo) =>{
+        const taskItem = document.createElement('li');
+        taskItem.className = 'task-item';
+
+        taskItem.innerHTML= `<div class="checkmark">&#10004;</div>
+        ${todo.task}
+        <button class="delete-button">Delete Button</button>`;
+
+        if(todo.completed) {
+        taskItem.classList.add('checked')
+        complete.appendChild(taskItem)
+        } else {
+            incomplete.appendChild(taskItem)
+        }
+    })
+}
+
